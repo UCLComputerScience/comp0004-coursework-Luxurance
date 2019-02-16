831d71ff765f1b4975cf73052027ece31f364910
Luxurance <cr99415@icloud.com>
2019-02-16 19:42:41 +0000

    small progress

--- src/main/java/uk/ac/ucl/bag/LinkedListBag.java
+++ src/main/java/uk/ac/ucl/bag/LinkedListBag.java
@@ -16,6 +16,8 @@ public class LinkedListBag <T extends Comparable> extends AbstractBag<T>{
 
     private int maxSize;
 
+    private int curSize;
+
     private Element head;
 
     public LinkedListBag() throws BagException{
@@ -24,6 +26,39 @@ public class LinkedListBag <T extends Comparable> extends AbstractBag<T>{
 
     public LinkedListBag(int maxSize) throws BagException{
         this.maxSize = maxSize;
+        this.curSize = 0;
         this.head = null;
     }
+
+    public void add(T value) throws BagException{
+        if(head == null){
+            head = new Element(value, 1, null);
+            curSize++;
+        }
+        else{
+            Element curElement = head.next;
+            while(curElement.next != null){
+                if(curElement.value == value){
+                    curElement.occurrences++;
+                    return;
+                }
+                curElement = curElement.next;
+            }
+            if(curSize < maxSize) {
+                curElement.next = new Element(value, 1, null);
+                curSize++;
+            }
+            else{
+                throw new BagException("Bag is full");
+            }
+        }
+    }
+
+    public void addWithOccurrences(T value, int occurrences) throws BagException{
+        for(int i = 0; i < occurrences; i++){
+            add(value);
+        }
+    }
+
+
 }
