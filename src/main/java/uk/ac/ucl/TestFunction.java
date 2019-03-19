ca30833ace303f1e9dcc9524b8540624fcce4f42
Luxurance <cr99415@icloud.com>
2019-03-19 11:17:09 +0000

    This is the part three coursework but the same core program code in part two will be reused.

--- src/main/java/uk/ac/ucl/TestFunction.java
+++
@@ -1,9 +0,0 @@
-package uk.ac.ucl;
-
-public class TestFunction {
-    public static void main(String[] args){
-//        Patient patient = new Patient();
-        String a = "abcdr";
-        StringBuilder b = new StringBuilder();
-    }
-}
