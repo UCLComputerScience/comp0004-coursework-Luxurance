f4a8ae9be7273506afe2bbd8f045403229d9e6a3
Luxurance <cr99415@icloud.com>
2019-03-23 21:39:40 +0000

    improvement on code arrangement

--- src/main/java/uk/ac/ucl/servlets/ShowPatientList.java
+++ src/main/java/uk/ac/ucl/servlets/ShowPatientList.java
@@ -26,41 +26,33 @@ public class ShowPatientList extends HttpServlet
 
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException{
-        try {
-            // Code to use the model to process something would go here.
-            String url = request.getRequestURI();
-            System.out.println(url);
-            String[] urlEle = url.split("/");
-            System.out.println(urlEle.length);
-            if(urlEle.length == 2){
-                initialise(request,response);
-            }
-
-            String letter = request.getParameter("letterSelected");
-            if(letter != null) {
-                patientList = model.searchByStartLetter(letter);
-            }
-
-            Patient patient = model.searchById(request.getParameter("patientSelected"));
-            if(patient != null) {
-                patientInfo = model.getPatientInfo(patient);
-            }
-
-            if(request.getParameter("lastName") != null) {
-                patientList = model.searchPatient(request.getParameter("lastName"));
-            }
-
-            request.setAttribute("patientList",patientList);
-
-            request.setAttribute("patientInfo",patientInfo);
-
-            // Then forward to JSP.
-            ServletContext context = getServletContext();
-            RequestDispatcher dispatch = context.getRequestDispatcher("/displayList.jsp");
-            dispatch.forward(request, response);
+        // Code to use the model to process something would go here.
+        String url = request.getRequestURI();
+        System.out.println(url);
+        String[] urlEle = url.split("/");
+        System.out.println(urlEle.length);
+        if(urlEle.length == 2){
+            initialise(request,response);
         }
-        catch (IllegalAccessException e){ }
+        switch (urlEle[urlEle.length-1]){
+            case "letter":
+                sortByLetter(request,response);
+                break;
+            case "name":
+                showInfo(request,response);
+                break;
+            case "search":
+                searchByLastName(request,response);
+        }
+
+        request.setAttribute("patientList",patientList);
+
+        request.setAttribute("patientInfo",patientInfo);
+
+        // Then forward to JSP.
+        toJSP(request,response);
     }
+
 
     private void toJSP(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
         ServletContext context = getServletContext();
@@ -72,5 +64,29 @@ public class ShowPatientList extends HttpServlet
         model = new Model();
         model.readCSVFile("/Users/Lance 1 2/IdeaProjects/patient_data_web/patient_data/patients100.csv");
         patientList = model.getPatientList();
+    }
+
+    private void sortByLetter(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        String letter = request.getParameter("letterSelected");
+        if(letter != null) {
+            patientList = model.searchByStartLetter(letter);
+        }
+    }
+
+    private void showInfo(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+        try {
+            Patient patient = model.searchById(request.getParameter("patientSelected"));
+            if (patient != null) {
+                patientInfo = model.getPatientInfo(patient);
+            }
+        }
+        catch (IllegalAccessException e){}
+    }
+
+    private void searchByLastName(HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException{
+        if(request.getParameter("lastName") != null) {
+            patientList = model.searchPatient(request.getParameter("lastName"));
+        }
     }
 }
